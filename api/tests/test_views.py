5589fa79f285ffa1550cb5eae925be9ac8315829
montuyajoel <2025040@student.cct.ie>
2025-07-28 14:54:04 +0800

    feat: enable get and post endpoints for bookcatalogs

---
+++ api/tests/test_views.py
@@ -0,0 +1,41 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from api.models import Book
+
+
+class BookViewTest(APITestCase):
+    
+    def test_response_is_correct(self):
+        book = Book.objects.create(
+            **{
+                "title": "test_title",
+                "description": "test_description",
+                "author": "test_author"
+            }
+        )
+
+        url = reverse('api:books')
+        response = self.client.get(url, format='json')
+        assert response.status_code == status.HTTP_200_OK
+        body = response.json()
+        assert body== [          
+            {
+                "title": book.title,
+                "description": book.description,
+                "author": book.author,
+                "created_at": book.created_at.isoformat().replace("+00:00", "Z")               
+            }
+        ]
+
+    
+
+class HealthViewTest(APITestCase):
+    
+    def test_response_is_correct(self):
+        url = reverse('api:health')
+        response = self.client.get(url, format='json')
+        assert response.status_code == status.HTTP_200_OK
+        body = response.json()
+        assert body['status'] == 'OK'
+        assert body['status_message'] == 'Server is up and running.'
--- api/views.py
+++ api/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import BookSerializer
+from .models import Book
 
-# Create your views here.
+#Create an endpoint to perform healthcheck
+class HealthCheck(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'status': 'OK',
+            'status_message': 'Server is up and running.'
+        })
+health_view = HealthCheck.as_view()
+
+#
+# Enable endpoints for /api/books
+# get - retrieve all books
+# post - add new book to the database
+# delete - remove a book from the database
+#
+class BookView(APIView):
+    def get(self, request, *args, **kwargs):
+        all_books = Book.objects.all()
+        serializer = BookSerializer(all_books, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        
+        serializer = BookSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+    
+        
+book_view = BookView.as_view()
+
+
+
+
